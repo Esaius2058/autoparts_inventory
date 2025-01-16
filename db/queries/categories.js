8dc62a0b4782558a17aa1520cc277127eb9f0be1
Esaius2058 <wabwirewild@gmail.com>
2025-01-16 16:09:40 +0300

    Added functions for retrieving all categories, retreiving category by id, adding a new category, updating a category and deleting a specific category

---
+++ db/queries/categories.js
@@ -0,0 +1,92 @@
+const pool = require("../pool");
+
+async function getAllCategories() {
+  try {
+    const result = await pool.query("select * from categories");
+    return result.rows;
+  } catch (err) {
+    console.error("Error querying database: ", err);
+    throw err;
+  }
+}
+
+async function getCategoryById(id) {
+  try {
+    const result = await pool.query("select * from categories where categoryid = $1", [
+      id,
+    ]);
+
+    if (result.rows.length === 0) {
+      return { error: "Category not found", status: 404 };
+    }
+
+    return result.rows[0];
+  } catch (err) {
+    console.err("Error fetching category by Id:", err);
+    return { err: "An error occurred while fetching the part", status: 500 };
+  }
+}
+
+async function addCategory({ categoryid, categoryname }) {
+  if (!categoryid || !categoryname) {
+    throw new Error("Invalid input: All fields must be provided");
+  }
+
+  const query =
+    "insert into categories (categoryid, categoryname) values ($1, $2) returning categoryid";
+  const values = [categoryid, categoryname];
+
+  try {
+    const result = await pool.query(query, values);
+    return {
+      message: "Category added successfully",
+      categoryId: result.rows[0].categoryid,
+      status: 201,
+    };
+  } catch (err) {
+    console.error("Could not insert category:", err);
+    throw new Error("Database error: Could not insert category");
+  }
+}
+
+async function updateCategory({categoryid, categoryname}) {
+  if (!partId) {
+    throw new Error("categoryid is required to update a category");
+  }
+
+  const query =
+    "update categories set categoryname = $1 where categoryid = $2 returning *;";
+  const values = [categoryname, categoryid];
+
+  try {
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  } catch (err) {
+    console.error("Could not update category:", err);
+    throw new Error("Database error: Could not update category");
+  }
+}
+
+async function deleteCategory({ categoryid }) {
+  const query = "delete from categories where categoryid = $1";
+  const values = [categoryid];
+  try {
+    await pool.query(query, values);
+    return {
+      message: "Category added successfully",
+      categoryId: result.rows[0].categoryid,
+      status: 201,
+    };
+  } catch (err) {
+    console.error("Could not delete category:", err);
+    throw new Error("Database error: Could not delete category");
+  }
+}
+
+module.exports = {
+	getAllCategories,
+	getCategoryById,
+	addCategory,
+	updateCategory,
+	deleteCategory
+}
