dc6c1a28ca18385ce009769e95fab2ca2daf655f
Esaius2058 <wabwirewild@gmail.com>
2025-01-16 15:29:16 +0300

    created functions for getting all parts, getting a part by id, adding a new part, updating part details and deleting a part

---
+++ db/queries/parts.js
@@ -0,0 +1,98 @@
+const pool = require("../pool");
+
+async function getAllParts() {
+  try {
+    const result = await pool.query("select * from parts");
+    return result.rows;
+  } catch (err) {
+    console.error("Error querying database: ", err);
+    throw err;
+  }
+}
+
+async function getPartById(id) {
+  try {
+    const result = await pool.query("select * from parts where partid = $1", [
+      id,
+    ]);
+
+    if (result.rows.length === 0) {
+      return { error: "Part not found", status: 404 };
+    }
+
+    return result.rows[0];
+  } catch (err) {
+    console.err("Error fetching part by Id:", err);
+    return { err: "An error occurred while fetching the part", status: 500 };
+  }
+}
+
+async function addPart({ partname, description, price, categoryid }) {
+  if (!partname || !description || price == null || !categoryid) {
+    throw new Error("Invalid input: All fields must be provided");
+  }
+
+  const query =
+    "insert into parts (partname, description, price, categoryid) values ($1, $2, $3, $4) returning partid";
+  const values = [partname, description, price, categoryid];
+
+  try {
+    const result = await pool.query(query, values);
+    return {
+      message: "Part added successfully",
+      partId: result.rows[0].partId,
+      status: 201,
+    };
+  } catch (err) {
+    console.error("Could not insert part:", err);
+    throw new Error("Database error: Could not insert part");
+  }
+}
+
+async function updatePart({
+  partid,
+  partname,
+  description,
+  price,
+  categoryid,
+}) {
+  if (!partId) {
+    throw new Error("partId is required to update a part");
+  }
+
+  const query =
+    "update parts set partName = $1, description = $2, price = $3, categoryId = $4 where partId = $5 returning *;";
+  const values = [partname, description, price, categoryid, partid];
+
+  try {
+    const result = await pool.query(query, values);
+    return result.rows[0];
+  } catch (err) {
+    console.error("Could not update part:", err);
+    throw new Error("Database error: Could not update part");
+  }
+}
+
+async function deletePart({ partid }) {
+  const query = "delete from parts where partid = $1";
+  const values = [partid];
+  try {
+    await pool.query(query, values);
+    return {
+      message: "Part added successfully",
+      partId: result.rows[0].partId,
+      status: 201,
+    };
+  } catch (err) {
+    console.error("Could not delete part:", err);
+    throw new Error("Database error: Could not delete part");
+  }
+}
+
+module.exports = {
+	getAllParts,
+	getPartById,
+	addPart,
+	updatePart,
+	deletePart
+}
