893c1dc8534169e70310409d240ede72779a08b0
Esaius2058 <wabwirewild@gmail.com>
2025-01-17 12:10:29 +0300

    Changed the Pool to use a connectionString instead of the default object

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,7 @@
-const Pool = require("pg");
+const { Pool } = require("pg");
 
 const pool = new Pool({
-	connectionString: process.env.connectionString
+  connectionString: process.env.connectionString,
 });
 
 module.exports = pool;
