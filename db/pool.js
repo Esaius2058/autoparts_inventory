d85a542fb731d377cda62a3c66436ff116396bea
Esaius2058 <wabwirewild@gmail.com>
2025-01-18 10:23:26 +0300

    Added logic for validation and error handling

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,17 @@
 const { Pool } = require("pg");
 
+if (!process.env.connectionString) {
+  console.error("Error: connectionString environment variable is not set.");
+  process.exit(1); // Exit the application
+}
+
 const pool = new Pool({
   connectionString: process.env.connectionString,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
 });
 
 module.exports = pool;
