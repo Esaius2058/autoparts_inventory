f2ee556a71c7dfdfed828d3e086376265a6c455e
Esaius2058 <wabwirewild@gmail.com>
2025-01-19 21:09:07 +0300

    Configure app to use dynamic PORT from environment variables
    Log server listening message to confirm successful startup

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const express = require("express");
 const app = express();
 const atpRouter = require("./routers/atpRoutes");
 
-
-const env = process.env.NODE_ENV;
-const connectionString = process.env.connectionString;
-
-
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
